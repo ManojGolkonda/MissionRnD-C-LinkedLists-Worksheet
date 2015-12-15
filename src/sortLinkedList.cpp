327bcc69fdc992469e12a4811790d89e42aa9711
mayago <golkondamanoj@gmail.com>
2015-12-15 13:27:24 +0530

    Submit Commit

--- src/sortLinkedList.cpp
+++ src/sortLinkedList.cpp
@@ -18,6 +18,79 @@ struct node {
 	struct node *next;
 };
 
+void insertNodeBetween(struct node *start, struct node *end, struct node *middle)
+{
+	while (start != NULL)
+	{
+		if (middle->num >= start->num)
+		{
+			struct node *temp1 = middle->next;
+			struct node *temp2 = start->next;
+			start->next = middle;
+			start = middle;
+			start->next = temp2;
+			end->next = temp1;
+			break;
+		}
+		start = start->next;
+	}
+}
+
+void swap(struct node *a, struct node *b)
+{
+	int temp = a->num;
+	a->num = b->num;
+	b->num = temp;
+}
+
 struct node * sortLinkedList(struct node *head) {
-	return NULL;
+
+/*	struct node *s_min = head;	//pointer to sorted minimum
+	struct node *s_max = head;	//pointer to sorted maximum
+	while (s_max != NULL)
+	{
+		struct node *temp = s_max->next;
+		if (temp != NULL && temp->num < s_min->num)
+		{
+			struct node *temp1 = temp->next;
+			s_max->next = temp1;
+			temp->next = s_min;
+			s_min = temp;
+		}
+		else if (temp != NULL && temp->num > s_max->num)
+		{
+			s_max = s_max->next;
+		}
+		else
+		{
+			if (temp == NULL)
+				break;
+			insertNodeBetween(s_min, s_max, temp);
+		}
+	}
+	head = s_min;
+	return head;
+	*/
+	if (head == NULL)
+		return NULL;
+	int count, i;
+	struct node *temp;
+	struct node *start = NULL;
+	do
+	{
+		count = 0;
+		temp = head;
+
+		while (temp->next != start)
+		{
+			if (temp->num > temp->next->num)
+			{
+				swap(temp, temp->next);
+				count = 1;
+			}
+			temp = temp->next;
+		}
+		start = temp;
+	} while (count);
+	return head;
 }
