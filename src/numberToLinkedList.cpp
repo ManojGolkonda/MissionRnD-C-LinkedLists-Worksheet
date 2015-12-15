2d35fecc6265d07bba87424f9a0e8a48043f1510
mayago <golkondamanoj@gmail.com>
2015-12-15 11:21:01 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -6,7 +6,7 @@ INPUTS:  A number.
 
 OUTPUT: Create linked list from given number, each digit as a node.
 
-ERROR CASES: 
+ERROR CASES:
 
 NOTES: For negative numbers ignore negative sign.
 */
@@ -20,5 +20,28 @@ struct node {
 };
 
 struct node * numberToLinkedList(int N) {
-	return NULL;
+	if (!N)
+	{
+		struct node *new_node;
+		new_node = (struct node *)malloc(sizeof(struct node));
+		new_node->next = NULL;
+		new_node->num = N;
+		return new_node;
+	}
+	else
+	{
+		if (N < 0)
+			N *= -1;
+		struct node *current_node = NULL;
+		while (N > 0)
+		{
+			struct node *new_node;
+			new_node = (struct node *)malloc(sizeof(struct node));
+			new_node->next = current_node;
+			new_node->num = N % 10;
+			N /= 10;
+			current_node = new_node;
+		}
+		return current_node;
+	}
 }
